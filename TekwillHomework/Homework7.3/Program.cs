cc82b1adc32b9561258d661caa7782f7d20d9930
IonCucuietu <ioncucuietu10@gmail.com>
2023-07-13 20:29:44 +0300

    Homework7

---
+++ TekwillHomework/Homework7.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+     static void Main()
+     {
+          int marimeTabla = 8;
+          int[,] tabla = new int[marimeTabla, marimeTabla];
+
+         
+          for (int i = 0; i < marimeTabla; i++)
+          {
+               for (int j = 0; j < marimeTabla; j++)
+               {
+                    
+                    if ((i + j) % 2 == 0)
+                    {
+                         tabla[i, j] = 1; 
+                    }
+                    else
+                    {
+                         tabla[i, j] = 0; 
+                    }
+               }
+          }
+
+         
+          for (int i = 0; i < marimeTabla; i++)
+          {
+               for (int j = 0; j < marimeTabla; j++)
+               {
+                    Console.Write(tabla[i, j] + " ");
+               }
+               Console.WriteLine();
+          }
+
+          Console.ReadKey();
+     }
+}
